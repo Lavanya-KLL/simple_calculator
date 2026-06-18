4a790f663d277d347e0ab31885cadb064e37de33
Lavanya-KLL <kosurilavanya19@gmail.com>
2026-06-18 10:21:57 +0530

    Implement menu-driven calculator with history
    Refactored calculator to use a menu-driven interface with operation selection and history tracking.

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+history=[]
 def add(x,y):
     return x+y
 def subtract(x,y):
@@ -8,16 +9,36 @@ def divide(x,y):
     if y==0:
         return "Cant divide by zero"
     return x/y
-number1=float(input("Enter the first number: "))
-number2=float(input("Enter the second number: "))
-operator=input("Enter the operator(+,-,*,/): ")
-if operator=="+":
-    print(add(number1,number2))
-elif operator=="-":
-    print(subtract(number1,number2))
-elif operator=="*":
-    print(multiply(number1,number2))
-elif operator=="/":
-    print(divide(number1,number2))
-else:
-    print("Invalid opearator")
+    
+operations={
+        "1": ("+",add),
+        "2": ("-",subtract),
+        "3": ("*",multiply),
+        "4": ("/",divide)
+        }
+while True:
+    print("\n --- CALCULATOR")
+    print("(1) ADD")
+    print("(2) SUBTRACT")
+    print("(3) MULTIPLY")
+    print("(4) DIVIDE")
+    print("(5) SHOW HISTORY")
+    print("(6) EXIT")
+    
+    choice=input("ENTER CHOICE : ")
+     
+    if choice=="6":
+        print("GOODBYE!")
+        break
+    elif choice=="5":
+        print("\n---HISTORY---")
+        print("\n".join(history) if history else "No history yet!")
+    elif choice in operations:
+        a=float(input("Enter the first number: "))
+        b=float(input("Enter the second number: "))
+        symbol,func=operations[choice]
+        result=func(a,b)
+        print("RESULT : ",result)
+        history.append(f"{a} {symbol} {b} ={result}")
+    else:
+        print("Invalid choice")
